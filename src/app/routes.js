29136685dcc695e074ad0ba1223f218e710cfbb8
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-18 22:15:33 +0100

    Routes updated

--- src/app/routes.js
+++ src/app/routes.js
@@ -15,24 +15,6 @@ angular.module("MyBank", ["ngRoute"])
             templateUrl : "src/app/home/home.tpl.html"
         })
 
-        // Wine farms
-        .when("/wine_farms/:farm_id",
-        {
-            templateUrl : "src/app/wine_farms/farm.tpl.html"
-        })
-
-        // Products by category
-        .when("/categories/:category_id",
-        {
-            templateUrl : "src/app/products/category-products.tpl.html"
-        })
-
-        // Selected product details
-        .when("/categories/:category_id/:product_id",
-        {
-            templateUrl : "src/app/products/details.tpl.html"
-        })
-
         // redirect to home
         .otherwise(
         {
