42c888767706df6009077d9f87e93f01fd7bc056
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-18 22:09:48 +0100

    Initial commit

---
+++ src/app/navbar/NavbarController.js
@@ -0,0 +1,1 @@
+/*jslint browser: true, devel: true, nomen: true, plusplus: true, vars: true, white: true */
---
+++ src/app/routes.js
@@ -0,0 +1,41 @@
+/*jslint browser: true, devel: true, nomen: true, plusplus: true, vars: true, white: true */
+/*global angular*/
+
+angular.module("MyBank", ["ngRoute"])
+
+.config(["$routeProvider", function($routeProvider)
+{
+    "use strict";
+
+    $routeProvider
+
+        // Home
+        .when("/",
+        {
+            templateUrl : "src/app/home/home.tpl.html"
+        })
+
+        // Wine farms
+        .when("/wine_farms/:farm_id",
+        {
+            templateUrl : "src/app/wine_farms/farm.tpl.html"
+        })
+
+        // Products by category
+        .when("/categories/:category_id",
+        {
+            templateUrl : "src/app/products/category-products.tpl.html"
+        })
+
+        // Selected product details
+        .when("/categories/:category_id/:product_id",
+        {
+            templateUrl : "src/app/products/details.tpl.html"
+        })
+
+        // redirect to home
+        .otherwise(
+        {
+            redirectTo: "/"
+        });
+}]);
