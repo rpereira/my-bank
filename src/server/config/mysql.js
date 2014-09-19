bad38dc65c2793d33b6584108d47ae599f2aebd1
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-19 15:53:03 +0100

    MySQL config file

---
+++ src/server/config/mysql.js
@@ -0,0 +1,14 @@
+/*jslint browser: true, devel: true, nomen: true, plusplus: true, vars: true, white: true */
+/*global module */
+
+module.exports =
+{
+    "connection" :
+    {
+        "host"            : "localhost",
+        "user"            : "root",
+        "password"        : '',
+        "database"        : "MyBank",
+        "connectionLimit" : 100
+    }
+};
