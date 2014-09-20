3204e1777c6faa57e7f51f56b50d5c238d8d43a2
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-20 22:27:00 +0100

    App constants and global functions

---
+++ src/app/main.js
@@ -0,0 +1,45 @@
+/*jslint browser: true, devel: true, nomen: true, plusplus: true, vars: true, white: true */
+/*global angular, $*/
+
+angular.module("MyBank")
+
+.value("SERVICE_URL", "http://localhost:8080")
+
+.constant("categories",
+{
+    expenses:
+    [
+        "General",
+        "Food",
+        "Education",
+        "Clothes",
+        "Rent",
+        "House",
+        "Insurances",
+        "Health",
+        "Travel",
+        "Fuel",
+        "Car",
+        "Energy/Water"
+    ],
+    incomes:
+    [
+        "Wages",
+        "Renting",
+        "Gifts"
+    ]
+});
+
+function dismissModal(id)
+{
+    "use strict";
+
+    $(id).modal("hide");
+}
+
+function showModal(id)
+{
+    "use strict";
+
+    $(id).modal("show");
+}
