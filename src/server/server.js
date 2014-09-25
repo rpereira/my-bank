852e6ecc825fa1170b508cbb3de2f2dc88a13592
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-25 11:20:40 +0100

    Now using CORS middleware

--- src/server/server.js
+++ src/server/server.js
@@ -2,40 +2,31 @@
 /*global require, process */
 
 var express      = require("express");
+var cors         = require("cors");
 var logger       = require("morgan");
 var bodyParser   = require("body-parser");
 var errorHandler = require("errorhandler");
 
 var app = express();
-var SERVICE_URL = "http://127.0.0.1:55471";
 
 // all environments
 app.set("port", process.env.PORT || 8080);
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // development only
-if("development" == app.get('env'))
+if("development" == app.get("env"))
 {
     app.use(errorHandler());
 }
-
-app.all("/*", function(req, res, next)
-{
-    // CORS headers
-    res.header("Access-Control-Allow-Origin", SERVICE_URL);
-    //res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-
-    next();
-});
 
 // routes
 app.use('/', require("./app/routes.js"));
 
 // launch server
-app.listen(app.get('port'), function()
+app.listen(app.get("port"), function()
 {
     console.log("\n\n*********************************\n* Server listening on port " + app.get("port") + " *\n*********************************\n\n");
 });
